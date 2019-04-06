46d4b1a4b2843f3ff23baf6de94603c9c90f20bf
632844050 <632844050@qq.com>
2019-04-06 18:16:17 +0800

    再次重构，增加新特性

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,22 +8,19 @@
 			<button type="primary" @click="onShowDatePicker('time')">选择时间</button>
 			{{datetime}}
 			<button type="primary" @click="onShowDatePicker('datetime')">选择日期时间</button>
-			{{range}}
+			{{range[0]}} - {{range[1]}}
 			<button type="primary" @click="onShowDatePicker('range')">选择日期范围</button>
-			{{rangetime}}
+			{{rangetime[0]}} - {{rangetime[1]}}
 			<button type="primary" @click="onShowDatePicker('rangetime')">选择日期时间范围</button>
 		</view>
-		<mx-date-picker :show="showPicker" :type="type" :value="value" :show-seconds="true" @confirm="onSelected" @cancel="onSelected" />
-		<!-- <date-picker></date-picker> -->
+		<mx-date-picker :show="showPicker" :type="type" :value="value" :show-tips="true" :begin-text="'入住'" :end-text="'离店'" :show-seconds="true" @confirm="onSelected" @cancel="onSelected" />
 	</view>
 </template>
 <script>
-	import MxDatePicker from "@/components/mx-datepicker/mx-datepicker.vue";
-	import DatePicker from "@/components/date-picker/date-picker.vue";
+	import MxDatePicker from "@/components/mx-date-picker/mx-date-picker.vue";
 	export default {
 		components: {
-			MxDatePicker,
-			DatePicker
+			MxDatePicker
 		},
 		data() {
 			return {
@@ -33,7 +30,7 @@
 				datetime: '2019/01/01 15:00:12',
 				range: ['2019/01/01','2019/01/06'],
 				rangetime: ['2019/01/08 14:00','2019/01/16 13:59'],
-				type: 'date',
+				type: 'rangetime',
 				value: ''
 			}
 		},
@@ -47,7 +44,10 @@
 				this.showPicker = false;
 				if(e) {
 					this[this.type] = e.value; 
-					console.log(e);
+					//选择的值
+					console.log('value => '+ e.value);
+					//原始的Date对象
+					console.log('date => ' + e.date);
 				}
 			}
 		}
